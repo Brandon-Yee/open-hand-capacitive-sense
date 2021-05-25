194ec1fc7911a7cd1298efe0f6596dce62c43f7f
Brandon Yee <bky2@uw.edu>
2021-05-25 01:01:36 -0700

    added bag reader

---
+++ ROS/process_bag.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# process_bag.py
+# Reads ROS bag and generates graph
+
+import bagpy
+from bagpy import bagreader
+import pandas as pd
+
+b = bagreader('test.bag')
+
+# replace the topic name as per your need
+TF_MSG = b.message_by_topic(topic='/tf')
+CAP_MSG = b.message_by_topic(topic='/cap')
+tf = pd.read_csv(TF_MSG)
+cap = pd.read_csv(CAP_MSG)
